b71b98ac75b5f390f10a08fdfdf3031c1a2346cb
Sana <74452135+Sanakhan29@users.noreply.github.com>
2020-11-14 10:40:55 -0800

    Add files via upload

---
+++ week_4(task).py
@@ -0,0 +1,14 @@
+'''
+Ask user about his name,age,father_name
+PRint three different statements according
+to his age (10,20,30)
+'''
+name =input("Enter name:")
+Father_name =input("Enter Father name:")
+age =int(input("Enter age:"))
+
+if(age == 10) or (age == 20) or (age == 30):
+    print("Your age is:", age)
+   
+
+
